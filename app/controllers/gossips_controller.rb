6f91ef15b16449688dbf4b23a0b3df26f2a59ae7
TonyStark992 <maxime.castillo@gmail.com>
2020-05-07 19:37:27 +0200

    Suite projet jusqu'à destroy d'un gossip

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -16,4 +16,30 @@ class GossipsController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def update
+    @gossip = Gossip.find(params[:id])
+    if @gossip.update(gossip_params)
+      redirect_to @gossip
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
+    redirect_to welcome_path
+  end
+
+  private
+
+  def gossip_params
+    params.require(:gossip).permit(:title, :content)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'cities/show'
   get '/', to: 'landing_page#welcome'
   get 'welcome', to: 'landing_page#welcome'
   get 'welcome/:user', to: 'landing_page#welcome'
   get 'users/:id', to: 'users#show'
   get 'team', to: 'static_pages#team'
   get 'contact', to: 'static_pages#contact'
-  resources :gossips, only: [:show, :new, :create]
+  resources :gossips, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :cities, only: [:show]
 end
